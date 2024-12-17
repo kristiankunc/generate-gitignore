8602e25cfe0bf3b3f89e9620fa2da99438a8136f
Kristián Kunc <kristian.kunc@gmail.com>
2024-12-17 23:00:23 +0100

    test version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="generate-gitignore",
-    version="0.1.0",
+    version="0.1.1",
     author="Kristián Kunc",
     author_email="kristian@kristn.co.uk",
     description="A CLI tool for generating .gitignore files",
